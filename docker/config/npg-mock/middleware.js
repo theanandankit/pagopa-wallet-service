55566fe83583485fcb5d96106ecc1c5d3d914ab1
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-12-18 12:34:21 +0100

    Chk 2248 update api tests (#79)
    * refactor(api): return redirection url instead of form fields when creating session for APMs
    
    * refactor: add check for state returned by payment gateway when creating session
    
    * feat(apm): add wallet details for PayPal
    
    * feat(npg): update NPG API to 0.7
    
    * refactor(paypal): use a different client for PayPal wallet onboarding
    
    * fix: add detail for APM notify paypal
    
    * feat: handle notification request
    
    * refactor(api): add psp id in case of session creation with PayPal
    
    * fix(spec): fix typo in required field when creating session
    
    * feat: fix tests
    
    * fix: spotless
    
    * fix: spotless
    
    * feat: add api tests
    
    * feat: add test for notification without response
    
    * feat: add CR newman collection run
    
    * feat: handle npg build responses
    
    * fix: test collections and npg responses
    
    * fix: test file prefix
    
    * fix: add CD api tests
    
    * refactor(api): return redirection url instead of form fields when creating session for APMs
    
    * refactor: add check for state returned by payment gateway when creating session
    
    * feat(apm): add wallet details for PayPal
    
    * feat(npg): update NPG API to 0.7
    
    * refactor(paypal): use a different client for PayPal wallet onboarding
    
    * fix(spec): fix typo in required field when creating session
    
    * fix(apm): set wallet status to `VALIDATION_REQUESTED` after requesting APM onboarding
    
    * fix(api-spec): mark body as required in `createWalletSession` request
    
    * fix(serialization): add explicit `paymentMethodType` to `createSessionResponse`
    
    * fix: merge conflicts
    
    * fix: discriminator field
    
    * refactor(npg): remove check for `GDI_VERIFICATION` when onboarding cards
    
    * feat(npg): add check for fields list length when creating session for cards
    
    * fix: update npg build response
    
    * fix(api test paypal): expected return code
    
    * fix(api test paypal): change x-user-id
    
    ---------
    
    Co-authored-by: Giovanni Berti <giovanni.berti@pagopa.it>

--- docker/config/npg-mock/middleware.js
+++ docker/config/npg-mock/middleware.js
@@ -1,8 +1,24 @@
 module.exports = function (req, res, next) {
-    if (req.method === 'POST') {
-      // Converts POST to GET
-      req.method = 'GET'
-    }
-    // Continue to JSON Server router
-    next()
+  const db = req.app.db
+  if (req.method === 'POST') {
+    // Converts POST to GET
+    req.method = 'GET'
   }
+  const requestPath = req.path.toString();
+  const requestBody = req.body;
+  console.log(`Received request to path : ${requestPath} with body ${JSON.stringify(requestBody)}`)
+  if (requestPath.includes("build")) {
+    //handle build data request changing response based on input paymentService
+    const paymentService = requestBody.paymentSession.paymentService;
+    console.log(`Received build request for payment session: ${paymentService}`);
+    if (paymentService == "CARDS") {
+      res.json(db.get("buildCards"))
+    }
+    else {
+      res.json(db.get("buildApm"))
+    }
+  } else {
+    // Continue to default JSON Server router
+    next();
+  }
+}
